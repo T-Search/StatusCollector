cc674dca168774a78f53661e022b3d32fbb962ee
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-02-28 00:28:00 +0100

    First commit

---
+++ src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
@@ -0,0 +1,13 @@
+package de.tsearch.statuscollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusCollectorApplication.class, args);
+    }
+
+}
