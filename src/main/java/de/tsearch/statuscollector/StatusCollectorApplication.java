ed01d48fc7bbd80159cbe2cec88deb1ec75bc347
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-02-28 00:44:39 +0100

    Add after start runner method

--- src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
+++ src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
@@ -1,13 +1,25 @@
 package de.tsearch.statuscollector;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class StatusCollectorApplication {
 
+    private static final Logger LOGGER = Logger.getLogger(StatusCollectorApplication.class.getName());
+
     public static void main(String[] args) {
         SpringApplication.run(StatusCollectorApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner afterStartRunner() {
+        return args -> {
+        };
     }
 
 }
