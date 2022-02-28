30ce081f1d1e05c50ecaa2aadb50c955df916fcd
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-02-28 01:15:53 +0100

    Replace logger declaration with copy/paste safe solution

--- src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
+++ src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
@@ -1,16 +1,19 @@
 package de.tsearch.statuscollector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.logging.Logger;
+import java.lang.invoke.MethodHandles;
 
 @SpringBootApplication
 public class StatusCollectorApplication {
 
-    private static final Logger LOGGER = Logger.getLogger(StatusCollectorApplication.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
 
     public static void main(String[] args) {
         SpringApplication.run(StatusCollectorApplication.class, args);
