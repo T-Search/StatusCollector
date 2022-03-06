ebbfebf6e6a8794e3900585f16198d727dbf8c96
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-03-06 23:41:50 +0100

    Add import and remove broadcaster ids

--- src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
+++ src/main/java/de/tsearch/statuscollector/StatusCollectorApplication.java
@@ -1,11 +1,11 @@
 package de.tsearch.statuscollector;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import de.tsearch.statuscollector.database.redis.entity.Broadcaster;
+import de.tsearch.statuscollector.database.redis.entity.StreamStatus;
 import de.tsearch.statuscollector.database.redis.repository.BroadcasterRepository;
-import de.tsearch.statuscollector.service.twitch.StreamsService;
-import de.tsearch.statuscollector.service.twitch.WebhookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.lang.invoke.MethodHandles;
+import java.util.List;
+import java.util.Optional;
 
 @EnableScheduling
 @SpringBootApplication
@@ -26,8 +28,32 @@ public class StatusCollectorApplication {
     }
 
     @Bean
-    CommandLineRunner afterStartRunner(WebhookService webhookService, ObjectMapper objectMapper, StreamsService streamsService, BroadcasterRepository broadcasterRepository) {
+    CommandLineRunner addRemoveBroadcasterIds(@Value("#{'${import.broadcasterIds:}'.split(',')}") List<Long> addBroadcasters,
+                                              @Value("#{'${remove.broadcasterIds:}'.split(',')}") List<Long> removeBroadcasters,
+                                              BroadcasterRepository broadcasterRepository) {
         return args -> {
+            for (Long broadcasterId : addBroadcasters) {
+                if (broadcasterId == null) continue;
+                final Optional<Broadcaster> broadcasterOptional = broadcasterRepository.findById(broadcasterId);
+
+                if (broadcasterOptional.isEmpty()) {
+                    final Broadcaster broadcaster = new Broadcaster();
+                    broadcaster.setId(broadcasterId);
+                    broadcaster.setStatus(StreamStatus.OFFLINE);
+                    broadcasterRepository.save(broadcaster);
+                    LOGGER.info("Added broadcaster " + broadcasterId + " to database!");
+                }
+            }
+
+            for (Long broadcasterId : removeBroadcasters) {
+                if (broadcasterId == null) continue;
+                final Optional<Broadcaster> broadcasterOptional = broadcasterRepository.findById(broadcasterId);
+
+                if (broadcasterOptional.isPresent()) {
+                    broadcasterRepository.delete(broadcasterOptional.get());
+                    LOGGER.info("Removed broadcaster " + broadcasterId + " from database!");
+                }
+            }
         };
     }
 
