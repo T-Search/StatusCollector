76ada6ca0eb6e1e235bad7dc754d6cb72bbd8f47
Skillkiller <18444809+Skillkiller@users.noreply.github.com>
2022-03-25 01:14:53 +0100

    Update database scheman

--- src/main/java/de/tsearch/statuscollector/database/postgres/entity/Broadcaster.java
+++ src/main/java/de/tsearch/statuscollector/database/postgres/entity/Broadcaster.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -20,7 +17,7 @@ public class Broadcaster {
     private long id;
     @Column
     private String displayName;
-    @Column
+    @Enumerated(EnumType.STRING)
     private StreamStatus status;
     @Column
     private UUID twitchWebhookSecret;
--- src/main/java/de/tsearch/statuscollector/database/postgres/entity/TwitchMessageId.java
+++ src/main/java/de/tsearch/statuscollector/database/postgres/entity/TwitchMessageId.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -25,6 +22,7 @@ public class TwitchMessageId {
     private byte tries;
 
     @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Date incomeTime;
 
     public TwitchMessageId(String id, byte tries) {
